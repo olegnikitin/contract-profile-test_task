f952ec08108d76ce2d970a81161a90da80308328
Oleg Nikitin <olegnikitindev@gmail.com>
2022-10-30 19:38:11 +0200

    Made test green

---
+++ test/app.spec.js
@@ -0,0 +1,23 @@
+const app = require('../src/app');
+const axios = require('axios');
+const assert = require("assert");
+
+describe('App', function () {
+    let server;
+
+    before(function () {
+        server = app.listen(3001, () => {
+            console.log('Express App Listening on Port 3001');
+        });
+    });
+
+    after(function () {
+        server.close();
+    })
+
+    it('should return profile', async function () {
+        const result = await axios.get('http://localhost:3001/contracts/1');
+
+        assert.deepStrictEqual(result.data.id, 1);
+    });
+});
